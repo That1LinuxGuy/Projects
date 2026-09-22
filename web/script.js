d7d7313b6c4938d891eef7af132f41251c888b7e
mcallen <mcallen71398@gmail.com>
2026-09-22 11:20:59 -0500

    adding javascript to ibm-dev

---
+++ web/script.js
@@ -0,0 +1,21 @@
+function temperature(){
+    //To convert celsius to farenheit
+    //(CEL * 9/5) + 32
+    var c = document.getElementById("celsius").value;
+    var f = (c * 9/5) + 32
+    document.getElementById("fahrenheit").value = f
+}
+
+function weight(){
+    //To convert KGs to Pounds
+    // KG * 2.2
+    var kg = document.getElementById("kilo").value;
+    var p = kg * 2.2
+    document.getElementById("pounds").value = p
+}
+
+function distance(){
+    // convert KM to miles
+    var km = document.getElementById("kilometers").value;
+    var m = km * 0.062137
+    document.getElementById("miles").value = m
